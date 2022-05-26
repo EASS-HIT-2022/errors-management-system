212a268b9c6f16803020d1a2146d8d7bc6ae7b25
david rimon <davidr241194@gmail.com>
2022-05-26 13:09:00 +0300

    provide some comments

--- app/models.py
+++ app/models.py
@@ -4,14 +4,14 @@ from typing import Optional, List
 from uuid import UUID, uuid4
 import datetime
 
-
+'''All kinds of priority'''
 class Priority(str, Enum):
     blocker = "Blocker"
     high = "High"
     medium = "Medium"
     low = "Low"
 
-
+'''The basic information about error'''
 class Error(BaseModel):
     id: Optional[UUID] = uuid4()
     name: str
@@ -21,7 +21,7 @@ class Error(BaseModel):
     accept_date: datetime.date
     update_date: Optional[datetime.datetime]
 
-
+'''Only the parameters we should be able to change'''
 class ErrorUpdateRequest(BaseModel):
     name: Optional[str]
     priority: Optional[Priority]
